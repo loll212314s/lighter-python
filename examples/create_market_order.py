317a67c0b524d3792a406ad974573729323f9bd1
loll212314s <za37833555@gmail.com>
2025-10-25 22:45:17 +0200

    Update create_market_order.py

--- examples/create_market_order.py
+++ examples/create_market_order.py
@@ -7,9 +7,9 @@ logging.basicConfig(level=logging.DEBUG)
 # The API_KEY_PRIVATE_KEY provided belongs to a dummy account registered on Testnet.
 # It was generated using the setup_system.py script, and serves as an example.
 BASE_URL = "https://testnet.zklighter.elliot.ai"
-API_KEY_PRIVATE_KEY = "0xed636277f3753b6c0275f7a28c2678a7f3a95655e09deaebec15179b50c5da7f903152e50f594f7b"
-ACCOUNT_INDEX = 65
-API_KEY_INDEX = 3
+API_KEY_PRIVATE_KEY = "cc9c29835382fbd8d958c91cda7afb936e1f1b673ac37e52b43d92dfd07ee694a4f51ec0ca2b2f79"
+ACCOUNT_INDEX = 211
+API_KEY_INDEX = 2
 
 
 def trim_exception(e: Exception) -> str:
@@ -27,7 +27,7 @@ async def main():
     tx = await client.create_market_order(
         market_index=0,
         client_order_index=0,
-        base_amount=1000,  # 0.1 ETH
+        base_amount=0.0001,  # 0.1 ETH
         avg_execution_price=170000,  # $1700 -- worst acceptable price for the order
         is_ask=True,
     )
